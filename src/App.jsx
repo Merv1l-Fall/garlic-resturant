19d11522fec58723431805c396545c411a3761e3
Felix Skörvald <felix@skorvald.se>
2025-04-11 11:07:16 +0200

    Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
-import './App.css'
-import Menu from './components/Menu'
+import "./App.css";
+import Menu from "./pages/Menu";
+import { Outlet } from "react-router";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div>
-
-	</div>
-   
-  )
+    return (
+        <div>
+            <header>
+                <h1>KLYFTAN</h1>
+                <nav></nav>
+                <button>cart</button>
+            </header>
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createHashRouter } from "react-router";
+import "./index.css";
+import App from "./App.jsx";
+import Home from "./pages/Home.jsx";
+import Menu from "./pages/Menu.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createHashRouter([
+    {
+        path: "/",
+        Component: App,
+        children: [
+            { index: true, Component: Home },
+            { path: "/menu", Component: Menu },
+        ],
+    },
+]);
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
+);
