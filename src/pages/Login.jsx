9f38996bddfdd314d27c3482d655cbd899598a36
Felix Skörvald <felix@skorvald.se>
2025-04-13 19:23:27 +0200

    fully functional

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,47 @@
 import "./login.css";
+import { useLoginStore } from "../data/loginStore.js";
+import { useState } from "react";
 
 const Login = () => {
+    const [password, setPassword] = useState(null);
+    const toggleAdmin = useLoginStore((state) => state.toggleAdmin);
+    const admin = useLoginStore((state) => state.admin);
+
+    const handleLogIn = () => {
+        if (password === "mums") {
+            toggleAdmin();
+            console.log(!admin);
+            console.log("DU ÄR INNE");
+        } else {
+            console.log("FEEEEEEEl");
+        }
+        setPassword(null);
+    };
+
+    const handleLogOut = () => {
+        if (admin) {
+            toggleAdmin();
+            console.log(!admin);
+        } else {
+            console.log("du är redan utloggad");
+        }
+    };
+
     return (
         <div className="login">
             <div className="login-box">
                 <h3>Logga in för att ändra</h3>
-                <input type="text" id="password" placeholder="Lösenord" />
-                <button>Logga in</button>
+                <div className="input-box">
+                    <input
+                        type="password"
+                        id="password"
+                        placeholder="Lösenord"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password || ""}
+                    />
+                    <button onClick={handleLogIn}>Logga in</button>
+                    <button onClick={handleLogOut}>Logga ut</button>
+                </div>
             </div>
         </div>
     );
