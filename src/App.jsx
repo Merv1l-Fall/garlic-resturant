9e5a64ab699a35e6d0d89d2ab28472ba053ba0ed
Hanna Selden <hanna_lotta@hotmail.com>
2025-04-15 11:17:28 +0200

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,7 @@ function App() {
             <Header /> {/*Header*/}
             <main>
                 <Outlet />
-                {/* <EditMenu /> */}
-                <AddMenuItem />
-                <Outlet /> {/*Home och Menu */}
+               
             </main>
         </div>
     );
