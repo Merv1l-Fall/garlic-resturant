2f894465910e422536901fdf2d5ab3d26223dad4
Mikaela <mikaela.andersson@student.nbi-handelsakademin.se>
2025-04-17 21:17:29 +0200

    remove extra knapp

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -14,12 +14,14 @@ function CartItem({ item }) {
           {(item.quantity || 1)} × {item.price} kr
         </p>
       </div>
-      <button
-        onClick={() => removeFromCart(item.id)}
-        className="text-red-600 hover:underline text-sm"
-      >
-        Ta bort
-      </button>
+          {/*<button
+          onClick={() => removeFromCart(item.id)}
+          className="text-red-600 hover:underline text-sm"
+          >
+          Ta bort
+          </button>
+          */}
+
     </div>
   );
 }
