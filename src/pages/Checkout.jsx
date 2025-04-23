7eb181cbee99831010d7273e05250354a6f7f868
Felix Skörvald <felix@skorvald.se>
2025-04-23 11:28:13 +0200

    Receipt Back button fixed, some Css also

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,11 +1,12 @@
 import "./checkout.css";
 import { useCartStore } from "../data/cartStore"; 
 import { useState } from "react";
+import { useNavigate } from "react-router";
 
 
 const Checkout = () => {
+    const navigate = useNavigate();
     const cart = useCartStore((state) => state.cart);
-    console.log(cart);
     let total = cart.reduce( 
         (sum, item) => sum + (item.price * (item.quantity || 1)), 
         0 
@@ -15,23 +16,31 @@ const Checkout = () => {
 
     // SÄTT DENNA I KNAPPEN PÅ CART!!!
     const [orderNr, setOrderNr] = useState(Math.floor(Math.random()*100))
+
+
+    const handleNewOrder = () => {
+        useCartStore.setState({ cart: [] });
+        navigate("/menu");
+    }
         
     return (
         <div className="checkout">
             <div className="receipt">
 
                 <h2>Tack för din beställning</h2>
-                <h4>Ditt Ordernummer: {orderNr}</h4>
+                <h4>Ditt Ordernummer:</h4>
+                <h1>{orderNr}</h1>
                 <div className="items">
                     <div className="items-header">
                         <p>Beställning</p>
                         <p>Antal</p>
                         <p>Pris</p>
                     </div>
-                    {cart.map(item=>(                    <div className="item" key={item.title}>
+                    {cart.map(item=>(                    
+                    <div className="item" key={item.title}>
                         <p>{item.title}</p>
                         <p>{item.quantity}</p>
-                        <p>{item.price}</p>
+                        <p>{item.price*item.quantity}</p>
                     </div>))}
 
                 </div>
@@ -42,7 +51,7 @@ const Checkout = () => {
                     </div>
                     <h3>Total: {total}kr</h3>
                 </div>
-                <button>gå tillbaka</button>
+                <button onClick={handleNewOrder}>Ny beställning</button>
             </div>
         </div>
     );
