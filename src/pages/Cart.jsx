459a17717b5bd51a533d25b59296b1625558cf14
Mikaela <mikaela.andersson@student.nbi-handelsakademin.se>
2025-04-28 12:22:33 +0200

    change remove button

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -42,7 +42,7 @@ function Cart() {
                 <span>{item.quantity}</span>
                 <button onClick={() => handleIncrease(item.id)}>+</button>
                 <button className="remove-item" onClick={() => handleRemove(item.id)}>
-                  <img src="src/components/close.png" alt="Ta bort" className="remove-icon" />
+                <span className="remove-icon">🗙</span>
                 </button>
               </div>
             </div>
